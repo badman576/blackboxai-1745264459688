53de166aad5af00d17b0f0e6438d85940c3fa3a1
badman576 <ngarepeter570@gmail.com>
2025-04-22 01:05:31 +0300

    Add neurofusion-ultimate/main.py 1745273130189

---
+++ neurofusion-ultimate/main.py
@@ -0,0 +1,11 @@
+# NeuroFusion Ultimate main entry point
+
+from neurofusion import NeuroFusionUltimate
+
+def main():
+    ai_system = NeuroFusionUltimate()
+    ai_system.initialize()
+    ai_system.start()
+
+if __name__ == "__main__":
+    main()
